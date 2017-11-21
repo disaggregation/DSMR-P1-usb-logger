11e5b8437f690e8b603be6b9e621793a1ad599c7
DomoticX <info@domoticx.nl>
2017-11-21 18:24:09 +0100

    Update launch_scheduled_logger.sh
    Code optimisation

--- launch_scheduled_logger.sh
+++ launch_scheduled_logger.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-
-cd /
-cd home/pi/disaggregation/logger-DSMR-usb-P1/
-cd home/pi/disaggregation/DSMR-P1-usb-logger/
-sudo python schedule_p1_reader.py
-cd /
+sudo python /home/pi/disaggregation/DSMR-P1-usb-logger/schedule_p1_reader.py
